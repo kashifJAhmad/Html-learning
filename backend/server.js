8c5ba742eaec708967160b8dd01f75dd245dc1b6
kashifJAhmad <kashifjahmad@gmail.com>
2026-07-24 23:31:44 +0530

    Complete Student Tracker admin dashboard and authentication

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 
+
 dotenv.config();
 
 const userRoutes = require("./routes/userRoutes.js");
@@ -17,7 +18,7 @@ app.get("/", (req, res) => {
     res.send("Student Tracker Backend Running...");
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log(`🚀 Server Running on Port ${PORT}`);
